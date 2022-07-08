f0da1a060d28a5757ced3d03449c7e4668bbca23
carlos-sola <carlos.sola.z.95@gmail.com>
2022-07-08 18:49:15 -0300

    data base set up

--- api/src/models/Diet.js
+++ api/src/models/Diet.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 
 
 module.exports = (sequelize) => {
-  sequelize.define('diets', {
+  sequelize.define('diet', {
       id:{
         type: DataTypes.UUID,
         primaryKey: true
